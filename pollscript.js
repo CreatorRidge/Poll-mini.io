3ddeac67a4542a5eb2105f8a8858e15ea1de3372
Ridge <maddy.ab410@gmai.com>
2025-06-06 19:44:18 +0300

    script refactoring

--- pollscript.js
+++ pollscript.js
@@ -1,73 +1,53 @@
-let answers = document.querySelectorAll('.answer');
-let theme = document.querySelector('.game-site');
-let note = document.querySelector('.note');
-let themeButton = document.querySelector('.toggle-button');
+const answers = document.querySelectorAll('.answer');
+const theme = document.querySelector('.game-site');
+const note = document.querySelector('.note');
+const themeButton = document.querySelector('.toggle-button');
 
+function clearThemes() {
+    theme.classList.remove('neon-theme');
+    theme.classList.remove('light-theme');
+    theme.classList.remove('switch-theme');
+    theme.classList.remove('bright-theme');
+    theme.classList.remove('dark-theme');
+}
+
+function hideElements() {
+    themeButton.classList.remove('shown');
+    themeButton.classList.add('hidden');
+    note.classList.remove('shown');
+    note.classList.add('hidden');
+}
 
 for (let answer of answers) {
     answer.onchange = function() {
-    if (answer.dataset.evaluation === 'dark') {
-        theme.classList.remove('neon-theme');
-        theme.classList.remove('light-theme');
-        theme.classList.remove('switch-theme');
-        theme.classList.remove('bright-theme');
-        theme.classList.add('dark-theme');
-        themeButton.classList.remove('shown');
-        themeButton.classList.add('hidden');
-        note.classList.remove('shown');
-        note.classList.add('hidden');
-      } if (answer.dataset.evaluation === 'neon') {
-        theme.classList.remove('dark-theme');
-        theme.classList.remove('light-theme');
-        theme.classList.remove('switch-theme');
-        theme.classList.remove('bright-theme');
-        theme.classList.add('neon-theme');
-        themeButton.classList.remove('shown');
-        themeButton.classList.add('hidden');
-        note.classList.remove('shown');
-        note.classList.add('hidden');
-      } if (answer.dataset.evaluation === 'light') {
-        theme.classList.remove('dark-theme');
-        theme.classList.remove('neon-theme');
-        theme.classList.remove('switch-theme');
-        theme.classList.remove('bright-theme');
-        theme.classList.add('light-theme');
-        themeButton.classList.remove('shown');
-        themeButton.classList.add('hidden');
-        note.classList.remove('shown');
-        note.classList.add('hidden');
-      } if (answer.dataset.evaluation === 'switch') {
-        theme.classList.remove('dark-theme');
-        theme.classList.remove('neon-theme');
-        theme.classList.remove('light-theme');
-        theme.classList.remove('bright-theme');
-        theme.classList.add('switch-theme');
-        themeButton.classList.remove('hidden');
-        themeButton.classList.add('shown');
-        note.classList.remove('shown');
-        note.classList.add('hidden');
-      } if (answer.dataset.evaluation === 'bright') {
-        theme.classList.remove('dark-theme');
-        theme.classList.remove('neon-theme');
-        theme.classList.remove('light-theme');
-        theme.classList.remove('switch-theme');
-        theme.classList.add('bright-theme');
-        themeButton.classList.remove('shown');
-        themeButton.classList.add('hidden');
-        note.classList.remove('shown');
-        note.classList.add('hidden');
-      } if (answer.dataset.evaluation === 'personal') {
-        theme.classList.remove('neon-theme');
-        theme.classList.remove('light-theme');
-        theme.classList.remove('switch-theme');
-        theme.classList.remove('bright-theme');
-        theme.classList.add('dark-theme');
-        note.classList.remove('hidden');
-        note.classList.add('shown');
-        themeButton.classList.remove('shown');
-        themeButton.classList.add('hidden');
-      }
-  }
+        clearThemes();
+        hideElements();
+        
+        switch (answer.dataset.evaluation) {
+            case 'dark':
+                theme.classList.add('dark-theme');
+                break;
+            case 'neon':
+                theme.classList.add('neon-theme');
+                break;
+            case 'light':
+                theme.classList.add('light-theme');
+                break;
+            case 'switch':
+                theme.classList.add('switch-theme');
+                themeButton.classList.remove('hidden');
+                themeButton.classList.add('shown');
+                break;
+            case 'bright':
+                theme.classList.add('bright-theme');
+                break;
+            case 'personal':
+                theme.classList.add('dark-theme');
+                note.classList.remove('hidden');
+                note.classList.add('shown');
+                break;
+        }
+    }
 }
 
 themeButton.onclick = function() {
